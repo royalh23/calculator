9c94c172f11824cca34e5e26c05b06e05d123b06
Royal <royalhuseyn902@gmail.com>
2021-11-30 08:19:20 +0900

    Should fix the padding issue

--- script.js
+++ script.js
@@ -27,3 +27,40 @@ function operate(operator, a, b) {
       return divide(a, b);
   }
 }
+
+function displayValue(e) {
+  if (display.textContent === "0") {
+    display.textContent = e.target.textContent;
+  } else if (display.textContent !== "0") {
+      display.textContent += e.target.textContent;
+  }
+}
+
+// Create the display variable
+const display = document.querySelector(".display");
+
+// Create the variables for digits
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+const comma = document.querySelector("#comma");
+
+// Add event listeners to the digits
+one.addEventListener("click", displayValue);
+two.addEventListener("click", displayValue);
+three.addEventListener("click", displayValue);
+four.addEventListener("click", displayValue);
+five.addEventListener("click", displayValue);
+six.addEventListener("click", displayValue);
+seven.addEventListener("click", displayValue);
+eight.addEventListener("click", displayValue);
+nine.addEventListener("click", displayValue);
+zero.addEventListener("click", displayValue);
+comma.addEventListener("click", displayValue);
